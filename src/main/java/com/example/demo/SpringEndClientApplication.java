7795f72d22d2ff7a4cd62348a6356657a643e635
00850828 <ankit.shah@aa.com>
2018-02-01 15:48:15 -0600

    Done

--- src/main/java/com/example/demo/SpringEndClientApplication.java
+++ src/main/java/com/example/demo/SpringEndClientApplication.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class SpringEndClientApplication {
@@ -9,4 +12,18 @@ public class SpringEndClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEndClientApplication.class, args);
 	}
+}
+
+@RefreshScope
+@org.springframework.web.bind.annotation.RestController
+class RestController{
+	
+	@Value("${msg:Hello world - Config Server is not working..pelase check}")
+	private String msg;
+	
+	@RequestMapping("/msg")
+	public String showMessage(){
+		return msg+" Ankit";
+	}
+	
 }
